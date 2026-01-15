cf2f301bd991a3c615493b1c06ac474a084b33a0
DeepakSekar003 <deepaksekar003@gmail.com>
2026-01-15 19:10:41 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-echo "ApplicationStart started"
-
 cd /home/ubuntu/app
-
-# stop app if already running
 pm2 stop todo-app || true
 pm2 delete todo-app || true
-
-# start app
 pm2 start index.js --name todo-app
-
 pm2 save
 
-echo "ApplicationStart completed"
