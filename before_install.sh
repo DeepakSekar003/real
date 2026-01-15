99c5bb0973602b331a311a6d3a22565f19695493
DeepakSekar003 <deepaksekar003@gmail.com>
2026-01-15 17:41:37 +0530

    new file added

---
+++ before_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "BeforeInstall started"
+
+# create app directory if not exists
+mkdir -p /home/ubuntu/app
+
+# clean old files
+rm -rf /home/ubuntu/app/*
+
+echo "BeforeInstall completed"
