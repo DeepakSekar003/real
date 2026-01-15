82db7b94ec32041c428abd7d6de5a51038543a16
DeepakSekar003 <deepaksekar003@gmail.com>
2026-01-15 18:50:46 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-echo "BeforeInstall started"
-
-# create app directory if not exists
-mkdir -p /home/ubuntu/app
-
-# clean old files
-rm -rf /home/ubuntu/app/*
-
-echo "BeforeInstall completed"
+echo "BeforeInstall running"
