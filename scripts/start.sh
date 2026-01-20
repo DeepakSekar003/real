c24e62d3946691b41795253d8ccf57d67a05bb34
DeepakSekar003 <deepaksekar003@gmail.com>
2026-01-20 14:06:51 +0530

    made a changes

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -e
+
 cd /home/ubuntu/app
 
-pm2 stop all || true
-pm2 delete all || true
+pm2 stop node-app || true
+pm2 delete node-app || true
+
 pm2 start index.js --name node-app
 pm2 save
 
